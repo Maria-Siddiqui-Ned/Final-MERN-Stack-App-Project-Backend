817ecb1fa6d02c03a01584420882b5292c9148d7
Maria Siddiqui <mariasiddiqui_ned@yahoo.com>
2023-08-19 16:27:56 +0500

    backend Done

---
+++ api/users/model.js
@@ -0,0 +1,33 @@
+const {Schema, model} = require ('mongoose')
+
+const UserSchema  = new Schema ({
+    username : {
+        type : String ,
+        required : true 
+    },
+
+    email : {
+        type : String ,
+        required : true, 
+        unique: true
+    },
+
+    password : {
+        type : String ,
+        required : true 
+    },
+   
+    role: {
+        type: String,
+        required: true,
+        default: "user"
+    },
+    
+    signup_date : {
+        type : Date ,
+        default : Date.now 
+    }
+})
+
+const User = model ('user' , UserSchema)
+module.exports = User
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.SERVER_PORT
+// const mongoose = require('mongoose')
+const BrandRouter = require('./api/brands/router')
+const CategoryRouter = require('./api/category/router')
+const ProductRouter = require ('./api/products/router')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(
+  cors({
+    // origin : 'http://localhost:5176/',
+    credentials : true
+  })
+)
+app.use ('/api' , require('./api/users/router'))
+app.use('/api', BrandRouter)
+app.use('/api', CategoryRouter)
+app.use('/api', ProductRouter)
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//  mongoose.connect(process.env.MONGO_URL)
+//   .then(() => console.log("DB Connected Successfully"))
+//   .catch((err) => console.log(err.message))
